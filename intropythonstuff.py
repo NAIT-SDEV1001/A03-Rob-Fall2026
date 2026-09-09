7921c832d6869a5c0c42097d0ca9926437ebac4a
rbourbonnais123 <102501529+rbourbonnais123@users.noreply.github.com>
2026-09-09 06:54:03 -0600

    Add files via upload
    Initial demo file.

---
+++ intropythonstuff.py
@@ -0,0 +1,32 @@
+print("Intro Python Stuff \n\n")
+#Comment - these do not execute
+# to comment multiple lines, heightlite the lines and ctrl -/
+
+
+#Python is case sensitive. Spaces in code don't matter
+# Strings can be "" or ''
+print("Hi")
+print('Hi')
+
+#print It's a "groovy" day
+#We can use escape sequences/characters to fix this and
+# add behavior in a string
+
+print("It's a \"groovy\" day")
+
+#variables
+#a named container holds a value
+#python variables are dynamically typed
+#names must contain only letters, numbers, or _
+#names in python are snake_case
+
+name = "Rob"
+age = 23
+bank_account = 123.76
+
+name = input("What is your name?")
+print(name)
+name = "Rob"
+print(name)
+
+# f strings in the textbook.  This is for convience.
